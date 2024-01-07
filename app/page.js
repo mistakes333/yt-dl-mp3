b9c4d2803b0f76053b8fc8d8b86ab68f54e7be68
Mistakes 0fficial <113300685+mistakes333@users.noreply.github.com>
2024-01-07 17:54:21 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import axios from "axios";
 import { useRef, useState } from "react";
-import { youtube_parser } from "@mt333@utils";
+import { youtube_parser } from "@mt333/utils";
 import './globals.css';
 
 
