b1553ae7bc8b85c70a99ee70114a3e10a7bb2005
Mistakes 0fficial <113300685+mistakes333@users.noreply.github.com>
2024-01-08 01:35:32 +0700

    Update page.js

--- app/test/page.js
+++ app/test/page.js
@@ -1,1 +1,45 @@
+// test/page.js
 
+import { useRef, useState } from "react";
+import { youtube_parser } from "@mt333/utils";
+import './../globals.css';
+
+export default function Home() {
+  const inputUrlRef = useRef();
+  const [urlResult, setUrlResult] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const youtubeID = youtube_parser(inputUrlRef.current.value);
+
+    try {
+      const response = await fetch("/api/convert", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ youtubeID }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setUrlResult(data.link);
+      } else {
+        console.error("Failed to fetch");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+
+    inputUrlRef.current.value = "";
+  };
+
+  return (
+    <div className="app">
+      <section className="content">
+        {/* ... your existing JSX */}
+      </section>
+    </div>
+  );
+          }
+        
