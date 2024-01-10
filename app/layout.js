a6712dc91c33f9e8d45cd47bece97ad74a149881
Mistakes 0fficial <113300685+mistakes333@users.noreply.github.com>
2024-01-10 12:14:22 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,14 @@
 export const metadata = {
-  title: 'Youtube2mp3',
-  description: '',
-}
+  title: "Youtube2mp3",
+  description: "",
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  </head>
       <body>{children}</body>
     </html>
   )
